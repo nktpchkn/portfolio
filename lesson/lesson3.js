b3db720baf4a34503df8f1de8fed27afffcfb118
nktpchkn <ikei@mail.ru>
2022-10-13 17:24:50 +0300

    урок 3

---
+++ lesson/lesson3.js
@@ -0,0 +1,28 @@
+// string строка
+const drink = 'чай'
+console.log(drink)
+
+// number число
+const numberdrink = '5'
+console.log(numberdrink)
+// boolean тру или фалс
+const iscolddrink = false
+console.log(iscolddrink)
+
+// null пусто. неизвестно
+const srudentfavdrink = null 
+console.log(srudentfavdrink)
+// undefined значение не было присвоено. 
+let carOwner
+console.log(carOwner)
+// object обьект . сложный тип данных
+const napitok = {
+    drink: 'чай',
+    numberdrink: 5,
+    iscolddrink: true
+    
+}
+console.log(napitok)
+// symbol нужен для создания уник ключей обьекта
+const id =
+// bigint
