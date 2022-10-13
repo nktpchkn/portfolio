bf61398bb200d4bbac147bda91b8da57ab60d91b
nktpchkn <ikei@mail.ru>
2022-10-13 11:09:51 +0300

    урок 2

---
+++ lesson/lesson2.js
@@ -0,0 +1,12 @@
+
+var text = 'hello world!!!';
+console.log(text);
+
+let name = 'nick'
+const programminglanguage = 'JS'
+name = 'kek'
+text = 'hi'
+
+console.log(name)
+console.log(text)
+console.log(programminglanguage)
