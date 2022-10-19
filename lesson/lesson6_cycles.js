03a29612b0f37a204c108c4e11b80084ed1d2b00
nktpchkn <ikei@mail.ru>
2022-10-19 16:03:03 +0300

    урок 6

---
+++ lesson/lesson6_cycles.js
@@ -0,0 +1,36 @@
+// for 
+//while
+//do while
+
+// for(начальное знаяение;условие;шаг) {
+    
+// }
+
+// for(let i=0;i<10;i+=1){
+//     console.log('i=',i)
+// }
+
+// for(let i=10;i>0;i-=1){
+//     console.log('i',i)
+// }
+
+//while
+// while(условие){
+//     код
+// }
+// let i=0
+// while(i<10){
+//     console.log(i)
+//     i+=1
+// }
+
+//do while
+// do {
+
+// } while(уловие)
+let i=0
+do {
+    console.log('i',i)
+    i+=1
+
+    } while(i<=5)
